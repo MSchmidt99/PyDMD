fb9357228817e370033fbceda6d4412bff961e05
MSchmidt99 <Maxfinnian@gmail.com>
2023-02-07 01:05:11 -0500

    alternative approach

--- pydmd/zdecomposition.py
+++ pydmd/zdecomposition.py
@@ -1,63 +1,22 @@
 import warnings
 import copy
 
+from .utils import compute_tlsq, compute_svd
+
 import numpy as np
 import numba as nb
 from numba.types import bool_
-from scipy.optimize import minimize
+from sklearn.mixture import GaussianMixture
+warnings.filterwarnings("ignore", category=UserWarning, module='sklearn', lineno=1382)
 
 
 WARN_KWARGS = {
     'category': RuntimeWarning,
     'message': 'SVD optimal rank is 0. The largest singular values are '
-                'indistinguishable from noise. Setting rank truncation to 1.',
+               'indistinguishable from noise. Setting rank truncation to 1.',
     'module': r'pydmd',
     'lineno': 73
 }
-
-
-@nb.njit
-def first_false(arr, reverse=False, default=-1):
-    step = -1 if reverse else 1
-    start = (arr.shape[0] - 1) if reverse else 0
-    stop = -1 if reverse else arr.shape[0]
-    for idx in range(start, stop, step):
-        if arr[idx] == False:
-            return idx
-    return default
-
-
-@nb.njit
-def discard_small_corr(corr, Z, perturbation_thresh=0.001):
-    reverse = np.abs(Z) < 1
-    if reverse:
-        corr = corr[::-1]
-
-    for i in range(1, corr.shape[0]):
-        perturbation_distance = (
-            np.abs(corr[i] - corr[i-1])
-            if not (np.isnan(corr[i]) or np.isnan(corr[i-1]))
-            else 0
-        )
-        if perturbation_distance > perturbation_thresh:
-            corr[:i] = np.nan
-            break
-        if i == corr.shape[0] - 1:
-            corr[:] = np.nan
-    return corr[::-1] if reverse else corr
-
-
-@nb.njit
-def rolling_var(a):
-    a_pad = np.concatenate((
-        np.array([a[0]]),
-        a,
-        np.array([a[-1]]),
-    ))
-    return (
-        np.convolve(a_pad**2, np.array([1.,1.,1.]))
-        - (np.convolve(a_pad, np.array([1.,1.,1.]))**2 / 3)
-    )[2:-2]
 
 
 @nb.njit
@@ -68,224 +27,97 @@ def calculate_dynamics(Z, A, duration, offset):
     ) * A)
 
 
-@nb.njit
-def calculate_corr(signal, candidate_dynamics):
-    corr_denom = np.std(signal) * np.std(candidate_dynamics) * signal.shape[0]
-    return signal * candidate_dynamics / corr_denom
+def null_space(A):
+    u, s, vh = np.linalg.svd(A, full_matrices=True)
+    M, N = u.shape[0], vh.shape[1]
+    rcond = np.finfo(s.dtype).eps * max(M, N)
+    tol = np.amax(s) * rcond
+    num = np.sum(s > tol, dtype=int)
+    if num == M:
+        num = -1
+    Q = vh[num:,:].T.conj()
+    return Q
 
 
-@nb.njit
-def expand_window_midpoint(corr, window_midpoint, corr_thresh=-0.002):
-    nan_mask = np.isnan(corr)
-    outer_nan_mask = np.zeros(corr.shape, dtype=bool_)
-    outer_nan_mask[
-        first_false(nan_mask, default=0):
-        first_false(nan_mask, reverse=True, default=-2) + 1
-    ] = True
-    
-    corr_mask = ((corr > corr_thresh) | nan_mask) & outer_nan_mask
-    start = first_false(
-        corr_mask[:window_midpoint],
-        reverse=True, default=-2
-    ) + 1
-    end = first_false(
-        corr_mask[window_midpoint:],
-        default=corr.shape[0] - window_midpoint
-    ) + window_midpoint
-    
-    if np.all(nan_mask[start:end]):
-        return -1, -1
-    
-    start += first_false(nan_mask[start:]) // 2
-    end -= (end - first_false(nan_mask[:end], reverse=True) - 1) // 2
-    
-    return start, end
+def compute_phasor(Z, hdmd, signal, opt=True):
+    # swap from calculating phasor with snapshots[x] to whole signal (opt=True)
+    hdmd._sub_dmd._opt = opt
+    snp, _ = hdmd._col_major_2darray(signal)
+    snapshots = hdmd._pseudo_hankel_matrix(snp)
+    hdmd._sub_dmd._snapshots = snapshots
 
+    X = snapshots[:, :-1]
+    Y = snapshots[:, 1:]
 
-@nb.njit
-def expand_window_edges(corr, start, end, corr_thresh=-0.002):
-    nan_mask = np.isnan(corr)
-    outer_nan_mask = np.zeros(corr.shape, dtype=bool_)
-    outer_nan_mask[
-        first_false(nan_mask, default=0):
-        first_false(nan_mask, reverse=True, default=-2) + 1
-    ] = True
-    
-    corr_mask = ((corr > corr_thresh) | nan_mask) & outer_nan_mask
-    
-    start = first_false(
-        corr_mask[:start],
-        reverse=True, default=start
-    ) + 1
-    end = first_false(
-        corr_mask[end:],
-        default=corr.shape[0] - end
-    ) + end
-    
-    return start, end
+    sub_dmd = hdmd._sub_dmd
 
+    X, Y = compute_tlsq(X, Y, sub_dmd.tlsq_rank)
 
-def optimize_phasor(
-    signal, candidate_dynamics, candidate_exponential, start, end,
-    phi_bound=np.pi/4, amp_bound=10
-):
-    res = minimize(
-        lambda x: np.sum(
-            np.abs(
-                (
-                    np.diff(signal[start:end])
-                    - np.diff(
-                        (1j*np.sin(x[0]) + np.cos(x[0]))
-                        * x[1] * candidate_dynamics[start:end]
-                    )
-                ).real# / candidate_exponential[start:end] # avoids skewed min
-            )
-        ),
-        [0, 1],
-        bounds=[(-phi_bound, phi_bound), (1/amp_bound, amp_bound)]
+    operator = sub_dmd.operator
+
+    U, s, V = compute_svd(X, operator._svd_rank)
+
+    if operator._tikhonov_regularization is not None:
+        operator._norm_X = np.linalg.norm(X)
+    atilde = operator._least_square_operator(U, s, V, Y)
+
+    if operator._forward_backward:
+        # b stands for "backward"
+        bU, bs, bV = compute_svd(Y, svd_rank=len(s))
+        atilde_back = operator._least_square_operator(bU, bs, bV, X)
+        atilde = sqrtm(atilde.dot(np.linalg.inv(atilde_back)))
+
+    if operator._rescale_mode == 'auto':
+        operator._rescale_mode = s
+
+    operator._Atilde = atilde
+
+    if operator._rescale_mode is None:
+        # scaling isn't required
+        Ahat = operator._Atilde
+    elif isinstance(operator._rescale_mode, np.ndarray):
+        if len(operator._rescale_mode) != operator.as_numpy_array.shape[0]:
+            raise ValueError('''Scaling by an invalid number of
+                    coefficients''')
+        scaling_factors_array = operator._rescale_mode
+
+        factors_inv_sqrt = np.diag(np.power(scaling_factors_array, -0.5))
+        factors_sqrt = np.diag(np.power(scaling_factors_array, 0.5))
+
+        # if an index is 0, we get inf when taking the reciprocal
+        for idx, item in enumerate(scaling_factors_array):
+            if item == 0:
+                factors_inv_sqrt[idx] = 0
+
+        Ahat = np.linalg.multi_dot([factors_inv_sqrt, operator.as_numpy_array,
+                                    factors_sqrt])
+    else:
+        raise ValueError('Invalid value for rescale_mode: {} of type {}'
+                         .format(operator._rescale_mode,
+                                 type(operator._rescale_mode)))
+
+    operator._eigenvalues = np.array([Z])
+    operator._eigenvectors = null_space(
+        Ahat - Z * np.eye(Ahat.shape[0])
+    )[:,0][:,np.newaxis]
+
+    operator._compute_modes(Y, U, s, V)
+
+    sub_dmd._svd_modes = U
+    
+    sub_dmd._set_initial_time_dictionary(
+        {"t0": 0, "tend": snapshots.shape[1] - 1, "dt": 1}
     )
-    return res.x
 
+    A = sub_dmd._compute_amplitudes()[0]
 
-@nb.njit
-def get_candidateZ_corr_data(
-    signal, window_len, Z, Xi, t0, perturbation_thresh=0.001,
-    corr_thresh=-0.002, min_wavelength_match=0.5, wav_mag_thresh=0
-):
-    candidate_dynamics = calculate_dynamics(
-        Z,
-        Xi,
-        signal.shape[0],
-        t0
-    )
-    candidate_exponential = calculate_dynamics(
-        np.abs(Z),
-        np.abs(Xi),
-        signal.shape[0],
-        t0
-    ).real
-    # phase needs a slight nudge to begin, typically slightly off from Xi calc
-    data = np.array([0., 0.])
-    with nb.objmode(data='float64[:]'):
-        data += optimize_phasor(
-            signal,
-            candidate_dynamics,
-            candidate_exponential,
-            t0,
-            t0 + window_len
+    return np.mean(
+        hdmd.modes
+        / np.power(
+            np.repeat(Z, hdmd.d),
+            np.arange(hdmd.d)
         )
-    mod_phasor = (1j*np.sin(data[0]) + np.cos(data[0])) * data[1]
-    Xi *= mod_phasor
-    candidate_dynamics *= mod_phasor
-    candidate_exponential *= data[1]
-    candidate_real = candidate_dynamics.real
-    
-    small_signal_var = (rolling_var(signal) ** 0.5) < perturbation_thresh
-
-    corr = calculate_corr(signal, candidate_real)
-    corr[
-        np.abs(candidate_real) < (candidate_exponential / (np.sqrt(2)*3))
-    ] = np.nan
-    corr[small_signal_var] = np.nan
-    corr = discard_small_corr(corr, Z, perturbation_thresh=perturbation_thresh)
-
-    corr_range_start, corr_range_end = expand_window_midpoint(
-        corr,
-        t0 + (window_len // 2),
-        corr_thresh=corr_thresh
-    )
-
-    if corr_range_start == -1 and corr_range_end == -1:
-        return (
-            -1, # start
-            -1, # end
-            0, # len
-            np.inf, # window_error
-            np.array([np.nan]), # corr
-            1
-        )
-
-    period = 1 / (np.log(Z).imag / (2*np.pi))
-    envelope_peak = np.amax(
-        candidate_exponential[corr_range_start:corr_range_end]
-    )
-    if (
-        ((period * min_wavelength_match) > (corr_range_end - corr_range_start))
-        or (envelope_peak < wav_mag_thresh)
-    ):
-        return (
-            -1, # start
-            -1, # end
-            0, # len
-            np.inf, # window_error
-            np.array([np.nan]), # corr
-            1
-        )
-    
-    # fix phase
-    data = np.array([0., 0.])
-    with nb.objmode(data='float64[:]'):
-        data += optimize_phasor(
-            signal,
-            candidate_dynamics,
-            candidate_exponential,
-            corr_range_start,
-            corr_range_end
-        )
-    mod_phasor = (1j*np.sin(data[0]) + np.cos(data[0])) * data[1]
-    Xi *= mod_phasor
-    candidate_dynamics *= mod_phasor
-    candidate_exponential *= data[1]
-    candidate_real = candidate_dynamics.real
-    
-    _corr = calculate_corr(signal, candidate_real)
-    _corr = discard_small_corr(_corr, Z, perturbation_thresh=perturbation_thresh)
-    _corr[small_signal_var] = np.nan
-
-    corr_range_start, corr_range_end = expand_window_edges(
-        _corr,
-        corr_range_start,
-        corr_range_end,
-        corr_thresh=corr_thresh
-    )
-    
-    # fix phase again...
-    data = np.array([0., 0.])
-    with nb.objmode(data='float64[:]'):
-        data += optimize_phasor(
-            signal,
-            candidate_dynamics,
-            candidate_exponential,
-            corr_range_start,
-            corr_range_end
-        )
-    mod_phasor = (1j*np.sin(data[0]) + np.cos(data[0])) * data[1]
-    Xi *= mod_phasor
-    candidate_dynamics *= mod_phasor
-    candidate_exponential *= data[1]
-    candidate_real = candidate_dynamics.real
-    
-    # fix scale factor
-    scale = (
-        np.nanmean(signal[corr_range_start:corr_range_end]**2)
-        / np.nanmean(candidate_real[corr_range_start:corr_range_end]**2)
-    )**0.5
-    Xi *= scale
-    candidate_real *= scale
-
-    return (
-        corr_range_start,
-        corr_range_end,
-        corr_range_end - corr_range_start, # len
-        np.mean((
-            # use difference to improve matching in offset signals.
-            # copy needed for numba bug.
-            np.diff(signal[corr_range_start:corr_range_end].copy())
-            - np.diff(candidate_real[corr_range_start:corr_range_end].copy())
-        ) ** 2), # window_error
-        corr, # corr
-        Xi # new Xi
-    )
+    ) * A
 
 
 class ZDecomposition:
@@ -297,19 +129,14 @@ class ZDecomposition:
 
 
     def get_hankel_candidate_Zs(
-        self, signal, window_len, min_freq=0, max_freq=np.inf,
-        perturbation_thresh=0.001, corr_thresh=-0.002, min_wavelength_match=0.5,
-        wav_mag_thresh=0, use_diff=False
+        self, signal, window_len, min_freq=0, max_freq=np.inf
     ):
-        
         warnings.filterwarnings("error", **WARN_KWARGS)
         candidate_Zs = []
         noisy_window_count = 0
         for i in range(signal.shape[0] - window_len):
             try:
                 hdmd_signal_window = signal[i:i+window_len]
-                if use_diff:
-                    hdmd_signal_window = np.diff(hdmd_signal_window)
 
                 try:
                     hdmd = copy.deepcopy(self.HDMD_obj)
@@ -333,80 +160,109 @@ class ZDecomposition:
 
                 for d_idx in dynamics_idx:
                     Z = hdmd.eigs[d_idx]
-                    Xi = np.mean(
-                        hdmd.modes[:,d_idx]
-                        / np.power(
-                            np.repeat(Z, hdmd.d),
-                            np.arange(hdmd.d)[::-1]
-                        )
-                    ) * hdmd.amplitudes[d_idx]
-                    if use_diff:
-                        Xi /= (
-                            np.log(Z) # derivative scaling
-#                             * (Z ** -0.5) # half step shift for midpoint centered difference
-                        )
-
-                    corr_data = get_candidateZ_corr_data(
-                        signal,
-                        window_len,
-                        Z,
-                        Xi,
-                        i,
-                        perturbation_thresh=perturbation_thresh,
-                        corr_thresh=corr_thresh,
-                        min_wavelength_match=min_wavelength_match,
-                        wav_mag_thresh=wav_mag_thresh
-                    )
+                    if Z.imag < 0:
+                        # reduce duplication through conjugates
+                        Z = np.conjugate(Z)
                     
                     candidate_Zs.append({
                         'Z': Z,
-                        'A': hdmd.amplitudes[d_idx],
-                        'M': hdmd.modes[:,d_idx],
-                        'Xi': corr_data[5],
-                        't0': i,
-                        'start': corr_data[0],
-                        'end': corr_data[1],
-                        'len': corr_data[2],
-                        'window_error': corr_data[3],
-                        'corr': corr_data[4]
+                        'start': i,
+                        'end': i + window_len
                     })
             except RuntimeWarning as w:
                 noisy_window_count += 1
         return candidate_Zs, noisy_window_count
     
     def sift(
-        self, signal, window_len, min_freq=0, max_freq=np.inf,
-        perturbation_thresh=0.001, corr_thresh=-0.002, min_wavelength_match=0.5,
-        wav_mag_thresh=0, use_diff=False
+        self, signal, window_len, min_freq=0, max_freq=np.inf, min_windows=10,
+        noise_var_tol=0.001, uniqueness_tol=1e-6
+        
     ):
         candidate_Zs, noisy_window_count = self.get_hankel_candidate_Zs(
-            signal, window_len, min_freq=min_freq, max_freq=max_freq,
-            perturbation_thresh=perturbation_thresh, corr_thresh=corr_thresh,
-            min_wavelength_match=min_wavelength_match, wav_mag_thresh=wav_mag_thresh,
-            use_diff=use_diff
+            signal, window_len, min_freq=min_freq, max_freq=max_freq
         )
-
-        errors = np.array([
-            candidate_Z['window_error'] / candidate_Z['len']
-            if candidate_Z['len'] else np.inf
-            for candidate_Z in candidate_Zs
-        ])
-        best_Z_idx = np.argmin(errors)
-
-        Z = candidate_Zs[best_Z_idx]
         
-        if Z['len'] == 0:
-            warnings.warn("No optimal Z found.")
-            return Z, signal
+        self.Z_spectrogram = np.array([
+            # [time, real, imag]
+            [Z['start'], Z['Z'].real, Z['Z'].imag]
+            for Z in candidate_Zs
+        ])
 
-        Z_dynamics = calculate_dynamics(
-            Z['Z'],
-            Z['Xi'],
-            signal.shape[0],
-            Z['t0']
-        ).real
+        # identify unique Z's
+        components = np.arange(1, window_len * 2)
+        bics = []
+        mixtures = []
+        for n_c in components:
+            gm = GaussianMixture(n_components=n_c, random_state=0).fit(self.Z_spectrogram)
+            bics.append(gm.bic(self.Z_spectrogram))
+            mixtures.append(gm)
 
+        self.Z_gaussians = {}
+        cluster_model = mixtures[np.argmin(bics)]
+        pred = cluster_model.predict(self.Z_spectrogram)
+        for cluster in np.unique(pred):
+            cluster_Z = self.Z_spectrogram[pred == cluster]
+            if cluster_Z.shape[0] >= min_windows:
+                self.Z_gaussians[cluster] = cluster_Z
+
+        # optionally filter Z_gaussians
+        for cluster in list(self.Z_gaussians.keys()):
+            cluster_Z = self.Z_gaussians[cluster]
+            if np.std(cluster_Z[:,1]) * np.std(cluster_Z[:,2]) > noise_var_tol:
+                del self.Z_gaussians[cluster]
+        
+        # identify and merge duplicate Z values
+        Z_values = np.array([
+            np.mean(cluster_Z[:, 1]) + 1j*np.mean(cluster_Z[:, 2])
+            for cluster_Z in self.Z_gaussians.values()
+        ])
+        clusters = list(self.Z_gaussians.keys())
+        for i in range(len(Z_values)):
+            if i >= len(Z_values):
+                break
+            sim_mask = np.abs(Z_values - Z_values[i]) < uniqueness_tol
+            if sum(sim_mask) > 1:
+                argwhere_sim = np.argwhere(sim_mask).flatten()
+                arg_duplicates = argwhere_sim[argwhere_sim > i]
+                for j in arg_duplicates[::-1]:
+                    self.Z_gaussians[clusters[i]] = np.concatenate([
+                        self.Z_gaussians[clusters[i]],
+                        self.Z_gaussians[clusters[j]]
+                    ])
+                    del self.Z_gaussians[clusters[j]]
+                    clusters.pop(j)
+                    Z_values = np.delete(Z_values, j, 0)
+
+        # itentify Z spans and create Z dicts
         residual = signal.copy()
-        residual[Z['start']:Z['end']] -= Z_dynamics[Z['start']:Z['end']]
+        Zs = []
+        for cluster, cluster_Z in self.Z_gaussians.items():
+            Z = np.mean(cluster_Z[:, 1]) + 1j*np.mean(cluster_Z[:, 2])
+            start = int(np.amin(cluster_Z[:, 0])) + 1
+            end = int(np.amax(cluster_Z[:, 0])) + window_len
+            length = end - start
+            Xi = compute_phasor(
+                Z,
+                copy.deepcopy(self.HDMD_obj),
+                signal[start:end]
+            )
+            dynamics = calculate_dynamics(Z, Xi, length, 0)
+            scale, offset = np.linalg.lstsq(
+                np.vstack([dynamics.real, np.ones(length)]).T,
+                signal[start:end],
+                rcond=None
+            )[0]
+            Xi *= scale
+            dynamics *= scale
+            residual[start:end] -= (dynamics.real + offset)
+            Zs.append({
+                'Z': Z,
+                'Xi': Xi,
+                'offset': offset,
+                'start': start,
+                'end': end,
+                'len': end - start,
+                'dynamics': dynamics
+            })
 
-        return Z, residual
+        return Zs, residual
